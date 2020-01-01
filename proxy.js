e998e88ec4a72f050d38a581eb830939bd4c5917
ZHOUYIJIEQM <husiluanxianguser@163.com>
2020-01-01 17:07:40 +0800

    用别人搭建的数据，老是图片403，这次用node本地代理

---
+++ proxy.js
@@ -0,0 +1,46 @@
+const http = require('http');
+const request = require('request');
+
+const hostName = '127.0.0.1';
+const port = 8010;
+const imgPort = 8011;
+
+// 创建http服务器
+const apiServer = http.createServer((req, res) => {
+  const url = 'http://news-at.zhihu.com/api/4' + req.url;
+  const options = {
+    url: url
+  };
+  function callback (error, response, body) {
+    if(!error && response.statusCode === 200){
+      res.setHeader('Content-Type', 'text/plain;charset=UTF-8');
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.end(body);
+    }
+  }
+  request.get(options, callback);
+});
+apiServer.listen(port, hostName, () => {
+  console.log(`代理运行在: http://${hostName}:${port}`);
+});
+
+// 图片代理
+const imgServer = http.createServer((req, res) => {
+  const url = req.url.split('/img/')[1];
+  const options = {
+    url: url,
+    encoding: null
+  };
+  function callback(error, response, body){
+    if(!error && response.statusCode === 200){
+      const contentType = response.headers['content-type'];
+      res.setHeader('Content-Type', contentType);
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.end(body);
+    }
+  }
+  request.get(options, callback);
+});
+imgServer.listen(imgPort, hostName, () => {
+  console.log(`图片代理运行在: http://${hostName}:${imgPort}/`);
+})
