15c8294964dec09eb5880bf9c2a085efe82ce455
root <root@vultr.guest>
2020-01-09 12:19:16 +0800

    改回本地接口

--- src/api/proxyPort.js
+++ src/api/proxyPort.js
@@ -1,8 +1,4 @@
 export default {
-  apiPath: 'http://139.180.164.150:8010/',
-  imgPath: 'http://139.180.164.150:8011/img/'
+  apiPath: 'http://127.0.0.1:8010/',
+  imgPath: 'http://127.0.0.1:8011/img/'
 }
-// export default {
-//   apiPath: 'http://127.0.0.1:8010/',
-//   imgPath: 'http://127.0.0.1:8011/img/'
-// }
